9abf1b6592f8b6886f1afab4ffeb68fc90e94153
Kevin Huang <k11huang@torontomu.ca>
2025-08-04 12:37:21 -0400

    Created endpoints for json savedata

---
+++ README.md
@@ -0,0 +1,7 @@
+Backend for godot game I am making :)
+
+FastAPI, Uvicorn, SwaggerUI
+
+Planning on using Render Free Plan to deploy
+
+Run with: fastapi dev main.py
--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message: Hello World!"}
+@app.get("/savedata/{user_id}")
+async def read_save_json(user_id: int):
+    return {"Retribed save json for user " + user_id}
+
+@app.put("/savedata/{user_id}")
+async def update_save_json(user_id: int):
+    return {"Updated save json for user " + user_id}
+
+@app.post("/savedata/{user_id}")
+async def new_save_json(user_id: int):
+    return {"Created new save json for user " + user_id}
